674bfddbf4abc3d6888c01a3cfc14098ba35ac17
Mike Harrison <mike.harrison@custompermits.com>
2019-09-14 14:20:15 -0400

    feat: use environment variable for API_BASE

--- src/renderer/redux/modules/services/constants.js
+++ src/renderer/redux/modules/services/constants.js
@@ -1,1 +1,3 @@
-export const HOME = 'Home';
+export const API_BASE = process.env.API_BASE || 'https://mofb-api.appspot.com';
+export const API_PREFIX = '/api/v2';
+export const API_URL = API_BASE + API_PREFIX;
