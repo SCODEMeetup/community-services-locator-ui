f9203528400ca3c777723aa2bd08a8dab000599c
Mike Harrison <mike.harrison@custompermits.com>
2019-09-15 19:22:27 -0400

    change: if map view is accessed directly via url set the store on load

--- src/renderer/scenes/map/mapClass.jsx
+++ src/renderer/scenes/map/mapClass.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Flexbox from 'flexbox-react';
 import Map from 'containers/map';
-import { router } from 'src/renderer';
+import { router, store } from 'src/renderer';
 import PropTypes from 'prop-types';
+import {
+  openCategory,
+  openSubCategory,
+  selectedServices,
+} from 'redux-modules/services/paths';
+import { setstate, select } from 'redux-modules/general';
 
 export default class Home extends React.Component {
   static propTypes = {
@@ -11,9 +17,37 @@ export default class Home extends React.Component {
 
   componentDidMount() {
     const route = router.getState();
-    const { sub } = route.params;
-    if (sub) {
-      this.props.getServiceLocations(sub, true);
+    const { cat, sub } = route.params;
+    if (cat) {
+      // set the store open category
+      store.dispatch(setstate(cat, openCategory));
+
+      if (sub) {
+        // set the store open subcategory
+        store.dispatch(setstate(sub, openSubCategory));
+
+        const currentServices = select(selectedServices, store.getState());
+
+        const selectedSubs = currentServices[cat];
+
+        // if we dont have any selected sub categories in the store
+        // we need to set the subcategory manually
+        // this only happens if the url is accessed directly
+        if (!selectedSubs || !selectedSubs[sub]) {
+          store.dispatch(
+            setstate(
+              {
+                [cat]: {
+                  [sub]: true,
+                },
+              },
+              selectedServices
+            )
+          );
+        }
+
+        this.props.getServiceLocations(sub, true);
+      }
     }
   }
 
